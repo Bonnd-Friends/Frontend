9b958bb89889732112ff005578c6b3efe31cd81a
Nishantth1 <nishantthakre9@gmail.com>
2023-11-17 23:01:14 +0530

    Updated Theme for the App

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: 'var(--black1)',
+        black2: 'var(--black2)',
+        black3: 'var(--black3)',
+        black4: 'var(--black4)',
+        black5: 'var(--black5)',
+        black6: 'var(--black6)',
+        white: 'var(--white)',
+        background: 'var(--background)',
+        pink_bg: 'var(--pink_bg)',
+      },
+    },
   },
   plugins: [],
 }
